d5249ba20f4fa415c092f5019c3cfc1e2335ae27
Abram <git.skimming549@passinbox.com>
2026-03-31 02:00:06 +0100

    feat (backend): add EnvSettings model for environment vars

---
+++ api/utils/env_utils.py
@@ -0,0 +1,18 @@
+import os
+
+from pydantic import BaseModel
+
+
+class EnvSettings(BaseModel):
+    # Infrastructure Settings
+    DATABASE_URL: str = os.getenv(
+        "DATABASE_URL", "postgresql+asyncpg://abc:qwerty@db:5432/sentinel_db"
+    )
+
+    # Application Settings
+    JWT_KEY: str = os.getenv("JWT_KEY", "")
+    JWT_EXP: int = int(os.getenv("JWT_EXP", "900"))
+    JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM", "HS256")
+
+
+env = EnvSettings()
