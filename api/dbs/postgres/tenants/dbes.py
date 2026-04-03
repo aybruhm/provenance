e6e3e5d8a39e9f5e9439dda724137328d3ead3c9
Abram <git.skimming549@passinbox.com>
2026-04-04 00:00:30 +0100

    refactor (backend): add user_id column and index argument to tenants dbe

--- api/dbs/postgres/tenants/dbes.py
+++ api/dbs/postgres/tenants/dbes.py
@@ -1,10 +1,22 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, ForeignKey, Index, String
+from sqlalchemy.dialects.postgresql import UUID
 
 from dbs.postgres.shared.dbas import IDMixin, TimestampMixin
 
 
 class TenantDBE(IDMixin, TimestampMixin):
     __tablename__ = "tenants"
+    __table_args__ = (
+        Index(
+            "ix_tenants_user_id",
+            "user_id",
+        ),
+    )
 
     name = Column(String, nullable=False)
     policy_id = Column(String, nullable=False)
+    user_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+    )
