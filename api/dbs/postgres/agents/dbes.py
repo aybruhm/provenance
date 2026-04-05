e713f402094ce3dc73e991bce4c67f537aabb407
Abram <git.skimming549@passinbox.com>
2026-04-05 19:25:33 +0100

    chore (backend): add unique constriant for agents name & tenant_id

--- api/dbs/postgres/agents/dbes.py
+++ api/dbs/postgres/agents/dbes.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy import Column, ForeignKey, String, UniqueConstraint
 from sqlalchemy.dialects.postgresql import UUID
 from uuid_utils import uuid7
 
@@ -7,6 +7,13 @@ from dbs.postgres.shared.dbas import IDMixin, TimestampMixin
 
 class AgentDBE(IDMixin, TimestampMixin):
     __tablename__ = "agents"
+    __table_args__ = (
+        UniqueConstraint(
+            "name",
+            "tenant_id",
+            name="uq_agents_name_tenant_id",
+        ),
+    )
 
     name = Column(String, nullable=False)
     tenant_id = Column(
