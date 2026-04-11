9e6aacc746ae976f643b3f817fbbb420d9e72166
Abram <git.skimming549@passinbox.com>
2026-04-11 16:43:06 +0100

    feat (backend): implement policy DAO interface and implementation

---
+++ api/dbs/postgres/policy/dao.py
@@ -0,0 +1,47 @@
+from typing import Any, Dict
+from uuid import UUID
+
+from sqlalchemy import delete, select, update
+
+from dbs.postgres.engine import get_db_session
+from dbs.postgres.policy.dbes import PolicyDBE
+from dbs.postgres.policy.interfaces import PolicyDAOInterface
+
+
+class PolicyDAO(PolicyDAOInterface):
+    async def create_policy(self, policy_dbe: PolicyDBE) -> PolicyDBE:
+        async with get_db_session() as session:
+            session.add(policy_dbe)
+            await session.commit()
+            return policy_dbe
+
+    async def get_policy(self, policy_id: UUID) -> PolicyDBE | None:
+        async with get_db_session() as session:
+            result = await session.execute(
+                select(PolicyDBE).where(PolicyDBE.id == policy_id)
+            )
+            policy_dbe = result.scalar_one_or_none()
+            return policy_dbe
+
+    async def update_policy(
+        self,
+        policy_id: UUID,
+        values_to_update: Dict[str, Any],
+    ) -> PolicyDBE | None:
+        async with get_db_session() as session:
+            stmt = (
+                update(PolicyDBE)
+                .where(PolicyDBE.id == policy_id)
+                .values(values_to_update)
+                .returning(PolicyDBE)
+            )
+            result = await session.execute(stmt)
+            await session.commit()
+            updated_policy_dbe = result.scalar_one_or_none()
+            return updated_policy_dbe
+
+    async def delete_policy(self, policy_id: UUID) -> None:
+        async with get_db_session() as session:
+            stmt = delete(PolicyDBE).where(PolicyDBE.id == policy_id)
+            await session.execute(stmt)
+            await session.commit()
