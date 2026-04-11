e8f1aa43b1155fe4af982a863b04e9051bf71c3a
Abram <git.skimming549@passinbox.com>
2026-04-11 21:28:35 +0100

    refactor (backend): update PolicyService to support tenant policies

--- api/core/policy/service.py
+++ api/core/policy/service.py
@@ -3,11 +3,18 @@ from uuid import UUID
 from core.policy.dtos import CreatePolicyDTO, PolicyDTO, UpdatePolicyDTO
 from dbs.postgres.policy.dbes import PolicyDBE
 from dbs.postgres.policy.interfaces import PolicyDAOInterface
+from dbs.postgres.tenant_policies.dbes import TenantPolicyDBE
+from dbs.postgres.tenant_policies.interfaces import TenantPolicyInterface
 
 
 class PolicyService:
-    def __init__(self, dao: PolicyDAOInterface):
+    def __init__(
+        self,
+        dao: PolicyDAOInterface,
+        tenant_policy_dao: TenantPolicyInterface,
+    ):
         self.dao = dao
+        self.tenant_policy_dao = tenant_policy_dao
 
     def _map_dto_to_dbe(self, dto: CreatePolicyDTO | UpdatePolicyDTO) -> PolicyDBE:
         return PolicyDBE(
@@ -27,11 +34,32 @@ class PolicyService:
             updated_at=dbe.updated_at,  # type: ignore
         )
 
+    def _map_tenant_policy_dbe_to_dto(self, dbe: TenantPolicyDBE) -> PolicyDTO:
+        return PolicyDTO(
+            id=str(dbe.policy_id),
+            name=dbe.policy.name,  # type: ignore
+            version=dbe.policy.version,  # type: ignore
+            description=dbe.policy.description,  # type: ignore
+            rules=dbe.policy.rules.get("rules", []),  # type: ignore
+            created_at=dbe.policy.created_at,  # type: ignore
+            updated_at=dbe.policy.updated_at,  # type: ignore
+        )
+
     async def create_policy(self, create_dto: CreatePolicyDTO) -> PolicyDTO:
         dbe = self._map_dto_to_dbe(create_dto)
         policy_dbe = await self.dao.create_policy(policy_dbe=dbe)
         policy_dto = self._map_dbe_to_dto(dbe=policy_dbe)
         return policy_dto
+
+    async def get_tenant_policy(self, tenant_policy_id: UUID) -> PolicyDTO | None:
+        tenant_policy = await self.tenant_policy_dao.get_tenant_policy(
+            tenant_policy_id=tenant_policy_id
+        )
+        if not tenant_policy:
+            return None
+
+        tenant_policy_dto = self._map_tenant_policy_dbe_to_dto(dbe=tenant_policy)
+        return tenant_policy_dto
 
     async def get_policy(self, policy_id: UUID) -> PolicyDTO | None:
         policy_dbe = await self.dao.get_policy(policy_id=policy_id)
