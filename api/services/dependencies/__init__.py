a60bba898a731c6dd051ac98ff77694fe1bef3c6
Abram <git.skimming549@passinbox.com>
2026-04-25 19:15:26 +0100

    feat (backend): add dependency to get current scope from apikey

--- api/services/dependencies/__init__.py
+++ api/services/dependencies/__init__.py
@@ -2,3 +2,4 @@ from .auth import AuthDependencies
 
 auth_deps = AuthDependencies()
 get_current_user = auth_deps.get_current_user
+get_current_scope_from_apikey = auth_deps.get_current_scope_from_apikey
--- api/services/dependencies/auth.py
+++ api/services/dependencies/auth.py
@@ -1,7 +1,8 @@
 from fastapi import Depends, Request
 
+from middlewares.apikey_auth import APIKeyAuth
 from middlewares.jwt_cookie_auth import JWTCookieAuth
-from services.dependencies.types import CurrentUserContext
+from services.dependencies.types import CurrentScopeContext, CurrentUserContext
 from services.exceptions import UnauthorizedException
 
 
@@ -31,3 +32,29 @@ class AuthDependencies:
             token=token,
         )
         return user_context
+
+    @staticmethod
+    def get_current_scope_from_apikey(
+        request: Request,
+        token: str = Depends(APIKeyAuth()),
+    ) -> CurrentScopeContext:
+        """
+        Get the current authenticated user from the request using API key authentication.
+        """
+
+        user_id = getattr(request.state, "user_id", None)
+        username = getattr(request.state, "username", None)
+        scope = getattr(request.state, "scope", None)
+
+        if not user_id or not username:
+            raise UnauthorizedException(detail="User not authenticated")
+
+        if not scope:
+            raise UnauthorizedException(detail="Invalid api-key or revoked api-key")
+
+        scope_context = CurrentScopeContext(
+            id=user_id,
+            username=username,
+            scope=scope,
+        )
+        return scope_context
