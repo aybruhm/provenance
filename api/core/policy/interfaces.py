2dfc4011dc3335f9499e7366df081f739a78bf3e
Abram <git.skimming549@passinbox.com>
2026-04-30 17:52:37 +0100

    refactor (backend): move dao interfaces to api/core

--- api/core/policy/interfaces.py
+++ api/core/policy/interfaces.py
@@ -1,7 +1,35 @@
 from abc import ABC, abstractmethod
+from typing import Any, Dict
 from uuid import UUID
 
+from dbs.postgres.policy.dbes import PolicyDBE
 from dbs.postgres.tenant_policies.dbes import TenantPolicyDBE
+
+
+class PolicyDAOInterface(ABC):
+    @abstractmethod
+    async def create_policy(self, policy_dbe: PolicyDBE) -> PolicyDBE:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_policy(self, policy_id: UUID) -> PolicyDBE | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_policy_by_name(self, name: str) -> PolicyDBE | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def update_policy(
+        self,
+        policy_id: UUID,
+        values_to_update: Dict[str, Any],
+    ) -> PolicyDBE | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def delete_policy(self, policy_id: UUID) -> None:
+        raise NotImplementedError
 
 
 class TenantPolicyInterface(ABC):
